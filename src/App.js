173b855951505055f46dfaa55e2691d97f4fe8c1
ahsanofficial <bega95091gmail.com>
2022-09-19 11:03:28 +0530

    change_branch2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import './App.css';
-// import { Container } from '@chakra-ui/react'
 import AllRoutes from './Routes/AllRoutes'
 function App() {
   return (
-    // <Container className="App" backgroundColor={'red'} >
-    //   <flex h='100vh' py={20} backgroundColor={'black'}></flex>
-    //   <VStack w='full' p={10} spacing={10} align-alignItems='flex-start'></VStack>
-    //   <Heading size='2xl'>details</Heading>
-    //   <Text>ahsan</Text>
-    // </Container>
     <AllRoutes />
   );
 }
